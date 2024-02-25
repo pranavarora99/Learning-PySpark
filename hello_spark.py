a586194ca5b2d8e6e1073d43815eb65a0513009d
PRANAV ARORA <pranavarora99@mail.fresnostate.edu>
2024-02-25 01:48:08 -0500

    first python-spark project.
    configuring spark ecosystem and applying basic data transformations

---
+++ hello_spark.py
@@ -0,0 +1,32 @@
+import sys
+
+from pyspark.sql import *
+from lib.logger import Log4J
+from lib.utils import get_spark_app_config, load_customer_df, count_by_country
+
+if __name__ == "__main__":
+    conf = get_spark_app_config()
+    spark = SparkSession.builder \
+        .config(conf=conf) \
+        .getOrCreate()
+
+    spark.sparkContext.setLogLevel("INFO")
+    logger = Log4J(spark)
+
+    if len(sys.argv) != 2:
+        logger.error("Usage: HelloSpark <file_name>")
+        sys.exit(-1)
+
+    logger.info("Starting HelloSpark")
+
+    #conf_out = spark.sparkContext.getConf()
+    #logger.info(conf_out.toDebugString())
+
+    customer_survey_df = load_customer_df(spark, sys.argv[1])
+    partitioned_survey_df = customer_survey_df.repartitioned(2)
+    group_df = count_by_country(customer_survey_df)
+
+    group_df.show()
+
+    logger.info("Ending HelloSpark")
+    spark.stop()
---
+++ lib/utils.py
@@ -0,0 +1,25 @@
+import configparser
+
+from pyspark import SparkConf
+def get_spark_app_config():
+    spark_conf = SparkConf()
+    config = configparser.ConfigParser()
+    config.read("spark.conf")
+
+    for (key, val) in config.items("SPARK_APP_CONFIGS"):
+        spark_conf.set(key,val)
+
+    return spark_conf
+
+def load_customer_df(spark, data_file):
+    return spark.read \
+        .option("header", "true") \
+        .option("inferschema", "true") \
+        .csv(data_file)
+
+def count_by_country(customer_survey_df):
+     return customer_survey_df \
+        .where("Company == 'May Inc'") \
+        .select("Company", "Last Name", "Country") \
+        .groupBy("Country") \
+        .count()
