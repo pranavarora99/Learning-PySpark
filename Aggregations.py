3c238279fcc283671b897f02847eb78d2cf10614
PRANAV ARORA <43396873+pranavarora99@users.noreply.github.com>
2024-03-04 01:48:00 -0500

    Add files via upload

---
+++ Aggregations.py
@@ -0,0 +1,43 @@
+from pyspark.sql import SparkSession
+from lib.logger import Log4J
+from pyspark.sql import functions as f
+
+if __name__ == "__main__":
+    spark = SparkSession \
+        .builder \
+        .appName("Aggregations") \
+        .master("local[2]") \
+        .getOrCreate()
+
+    logger = Log4J(spark)
+
+    invoice_df = spark.read \
+        .format("csv") \
+        .option("header", "true") \
+        .option("inferSchema", "true") \
+        .load("Data/invoices.csv")
+
+    invoice_df.select(f.count("*").alias("count"),
+        f.sum("Quantity").alias("TotalQuantity"),
+        f.avg("UnitPrices").alias("AvgPrice"),
+        f.countDistinct("InvoiceNo").alias("DistinctInvoices")) \
+        .show()
+
+    invoice_df.createOrReplaceTempView("sales")
+    summary_sql = spark.sql("""
+        SELECT Country, InvoiceNo, 
+        sum(Quantity) as TotalQuantity, 
+        round(sum(Quantity * UnitPrice),2) as InvoiceValue
+        FROM sales GROUP BY Country, InvoiceNo
+    """)
+
+    summary_sql.show()
+
+    summary_df=invoice_df \
+        .groupBy("Country", "InvoiceNo") \
+        .agg(f.sum("Quantity").alias("TotalQuantity"),
+             f.round(f.sum(f.expr("Quantity * UnitPrice")),2).alias("InvoiceValue"),
+             f.expr("round(sum(Quantity * UnitPrice), 2) as InvoiceValueExpr")
+             )
+
+    summary_df.show()
